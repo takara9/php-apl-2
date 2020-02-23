0da6571260c5138243ea4edcbbc36e89d48a1aac
Maho Takara <takara9@gmail.com>
2020-02-23 14:52:05 +0900

    add menu item

--- menu.php
+++ menu.php
@@ -8,6 +8,15 @@
       <a href="form_search.php">検索</a>
     </div>
   </div>
+  <div class="dropdown">
+    <button class="dropbtn">模擬エラー発生</button>
+    <div class="dropdown-content">
+      <a href="">存在なしエラー</a>          
+      <a href="">アプリ内エラー</a>
+      <a href="">ランタイムエラー</a>
+    </div>
+  </div>
+
   <a href='view_host_info.php'>ホスト名表示</a>           
   <a href="view_phpinfo.php">phpinfo表示</a>
   <a href='form_login.php'>ログイン</a> 
